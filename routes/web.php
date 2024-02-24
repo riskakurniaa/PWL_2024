1f6f331d8a9f9ebd73ed96dfc428238b386458ac
Riska Kurnia Triwulandari <riskakurniatriwulandari@gmail.com>
2024-02-24 07:36:07 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('Selamat Datang');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2241720039 <br> Nama: Riska Kurnia Triwulandari';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/profile/{name}', function ($name) {
+    return "Nama Saya $name";
+})->name('profile');
+
+Route::get('/routeProfile', function () {
+    return to_route('profile', ['name' => 'Riska']);
+});
+
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
