05e165c3d7c10126d6772465d25db6de174431d0
Riska Kurnia Triwulandari <riskakurniatriwulandari@gmail.com>
2024-02-24 14:35:02 +0700

    View

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,4 +10,16 @@ class WelcomeController extends Controller
     {
         return 'Hello World';
     }
+
+    // public function greeting()
+    // {
+    //     return view('blog.hello', ['name' => 'Riska']);
+    // }
+
+    public function greeting()
+    {
+        return view('blog.hello')
+            ->with('name', 'Riska')
+            ->with('occupation', 'Astronaut');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -87,3 +87,13 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+// Route::get('/greeting', function () {
+//     return view('hello', ['name' => 'Riska']);
+// });
+
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'Riska']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
